4e58db30c248e92a3107e1553733e99296ea94ec
Kokid19 <mmwright619@gmail.com>
2026-02-03 14:46:08 -0700

    YAY MONSTER BEAN

--- off-the-grid/Scripts/Monster.cs
+++ off-the-grid/Scripts/Monster.cs
@@ -3,8 +3,9 @@ using System;
 
 public partial class Monster : CharacterBody3D
 {
-	protected float _wanderSpeed;
-	protected float _chaseSpeed;
+	private Player _player;
+	protected bool _playerSeen;
+	protected Vector3 _targetVelocity;
 	private NavigationAgent3D _navAgent;
 
 	// Called when the node enters the scene tree for the first time.
@@ -15,23 +16,38 @@ public partial class Monster : CharacterBody3D
 
 	private void _on_detection_body_entered(Node3D area)
 	{
-		if(area is Player player)
-        {
-            _navAgent.TargetPosition = Player.Instance.GlobalPosition;
-			Vector3 nextPoint = _navAgent.GetNextPathPosition();
-        }
-    }
+		if (area is Player player)
+		{
+			_player = player;
+			if (!player.Velocity.Equals(0))
+			{
+				_playerSeen = true;
+			}
+			else
+			{
+				_playerSeen = false;
+			}
+		}
+	}
+	private void _on_seen_range_body_exited(Node3D area) { if (area is Player) { _playerSeen = false; GD.Print(_player.Name); } }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector3 velocity = Velocity;
-		if (!IsOnFloor())
+		if (_playerSeen)
 		{
-			velocity += GetGravity() * (float)delta;
+			_navAgent.TargetPosition = _player.GlobalPosition;
+			Vector3 nextPoint = _navAgent.GetNextPathPosition();
+			_targetVelocity = (nextPoint - GlobalTransform.Origin).Normalized();
+			GD.Print(_player.Name);
 		}
+		else
+        {
+			_targetVelocity = new Vector3(0, 0, 0);
+        }
 
-		Velocity = velocity;
+		_targetVelocity = new Vector3(_targetVelocity.X, -9.8f, _targetVelocity.Z);
+		Velocity = Velocity.Lerp(_targetVelocity, 4f * (float)delta);
 		MoveAndSlide();
 	}
 }
