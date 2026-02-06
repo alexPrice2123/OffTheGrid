0c7dc0de9ac3525ec9b550bf4f3dd30685a35f35
Leo S <z.leosheng@gmail.com>
2026-02-06 12:21:31 -0700

    the floor on top is done

--- off-the-grid/Scripts/Ui.cs
+++ off-the-grid/Scripts/Ui.cs
@@ -19,45 +19,45 @@ public partial class Ui : Control
 	// Called when the node enters the scene tree for the first time.
 	public async override void _Ready()
 	{
-        _text = GetNode<Label>("Dialouge/Text");
-        _text.Text = _lineTable[_currentLine];
+		_text = GetNode<Label>("Dialouge/Text");
+		_text.Text = _lineTable[_currentLine];
 		_text.VisibleCharacters = 0;
 		_transitionMat = GetNode<ColorRect>("Transition").Material as ShaderMaterial;
 		await ToSignal(GetTree().CreateTimer(1.5), SceneTreeTimer.SignalName.Timeout);
-        Type();
-    }
+		Type();
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		_transitionMat.SetShaderParameter("fade", Mathf.Lerp((float)_transitionMat.GetShaderParameter("fade"), _transitionGoal, delta * 2));
 		if ((float)_transitionMat.GetShaderParameter("fade") >= 1)
-        {
-            GetTree().ChangeSceneToFile("res://Scenes/credits.tscn");
-        }
+		{
+			GetTree().ChangeSceneToFile("res://Scenes/credits.tscn");
+		}
 		if ((float)_transitionMat.GetShaderParameter("fade") <= 0)
-        {
+		{
 			_transitionGoal = 0;
-        }
+		}
 		if (_lineTable[_currentLine] == "Cutscene1" && !_inCutscene)
-        {
-            _inCutscene = true;
+		{
+			_inCutscene = true;
 			GetNode<Control>("Dialouge").Visible = false;
 			Cutscene1();
-        }
+		}
 		else if (_lineTable[_currentLine] == "Cutscene2" && !_inCutscene)
-        {
-            _inCutscene = true;
+		{
+			_inCutscene = true;
 			GetNode<Control>("Dialouge").Visible = false;
 			Cutscene2();
-        }
+		}
 		if (Input.IsActionJustPressed("Skip") && !_inCutscene)
 		{
 			if (GetNode<Control>("Dialouge").Visible == true)
-            {
+			{
 				if (GetParent().GetParent<fusePathHandler>()._isLight && !GetParent<Player>()._inTutorial)
-                {
-                    if (11 <= _currentLine && (_currentCount >= _lineTable[_currentLine].Length))
+				{
+					if (11 <= _currentLine && (_currentCount >= _lineTable[_currentLine].Length))
 					{
 						_transitionGoal = 1.1f;
 					}
@@ -75,9 +75,9 @@ public partial class Ui : Control
 						} 
 					} 
 				}
-                else
-                {
-                    if (8 <= _currentLine && (_currentCount >= _lineTable[_currentLine].Length))
+				else
+				{
+					if (8 <= _currentLine && (_currentCount >= _lineTable[_currentLine].Length))
 					{
 						GetNode<Control>("Dialouge").Visible = false;
 						GetParent<Player>()._currentScreenPos = GetParent<Player>()._defScreenPos;
@@ -97,29 +97,29 @@ public partial class Ui : Control
 							_text.Text = _lineTable[_currentLine];
 						} 
 					} 
-                }
-            }
+				}
+			}
 			else if (GetNode<Control>("Tutorial").Visible == true)
-            {
+			{
 				if (_tutPage > 4)
-                {
-                    GetNode<Control>("Tutorial").Visible = false;
+				{
+					GetNode<Control>("Tutorial").Visible = false;
 					GetParent<Player>()._inTutorial = false;
-                }
-                else
-                {
-                   GetNode<Label>("Tutorial/Text"+_tutPage).Visible = false;
+				}
+				else
+				{
+				   GetNode<Label>("Tutorial/Text"+_tutPage).Visible = false;
 					_tutPage++; 
-                }
-            }
+				}
+			}
 		}
 		GetNode<Label>("Dialouge/Warn").Visible = _currentCount >= _lineTable[_currentLine].Length;
 		if (Input.IsActionPressed("Skip") && !_inCutscene && !(GetParent().GetParent<fusePathHandler>()._isLight && !GetParent<Player>()._inTutorial))
-        {
-            _skipCount++;
+		{
+			_skipCount++;
 			if (GetNode<Control>("Dialouge").Visible == true)
-            {
-                if (_skipCount >= 10)
+			{
+				if (_skipCount >= 10)
 				{
 					GetNode<Label>("Dialouge/Warn").Visible = true;
 					GetNode<ProgressBar>("Dialouge/Bar").Visible = true;
@@ -132,10 +132,10 @@ public partial class Ui : Control
 					_skipCount = 0;
 					GetNode<Control>("Tutorial").Visible = true;
 				}
-            }
+			}
 			else if (GetNode<Control>("Tutorial").Visible == true)
-            {
-                if (_skipCount >= 10)
+			{
+				if (_skipCount >= 10)
 				{
 					GetNode<Label>("Tutorial/Warn").Visible = true;
 					GetNode<ProgressBar>("Tutorial/Bar").Visible = true;
@@ -145,8 +145,8 @@ public partial class Ui : Control
 					GetNode<Control>("Tutorial").Visible = false;
 					GetParent<Player>()._inTutorial = false;
 				}
-            }
-        }
+			}
+		}
 		else
 		{
 			_skipCount = 0;
@@ -155,27 +155,27 @@ public partial class Ui : Control
 		}
 		GetNode<ProgressBar>("Dialouge/Bar").Value = _skipCount;
 		GetNode<ProgressBar>("Tutorial/Bar").Value = _skipCount;
-    }
+	}
 
 	private void _on_return_button_up()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Captured;
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
 		GetTree().Paused = false;
 		GetNode<Control>("Pause").Visible = false;
 		GetNode<Control>("Crosshair").Visible = true;
-    }
+	}
 
 	private void _on_quit_button_up()
-    {
+	{
 		GetTree().Paused = false;
-        GetTree().ChangeSceneToFile("res://Scenes/menu.tscn");
-    }
+		GetTree().ChangeSceneToFile("res://Scenes/menu.tscn");
+	}
 
 	private void _on_controls_button_up()
-    {
+	{
 		GetNode<Sprite2D>("ControlSprite").Visible = true;
 		GetNode<Control>("Pause").Visible = false;
-    }
+	}
 
 	private void _on_back_button_up()
 	{
@@ -183,7 +183,7 @@ public partial class Ui : Control
 		GetNode<Control>("Pause").Visible = true;
 	}
 	
-    public async void Type()
+	public async void Type()
 	{
 		for (_currentCount = 0; _currentCount <= _lineTable[_currentLine].Length; _currentCount++)
 		{
@@ -191,19 +191,19 @@ public partial class Ui : Control
 			await ToSignal(GetTree().CreateTimer(_typingSpeed), SceneTreeTimer.SignalName.Timeout);
 		}
 		_text.VisibleCharacters = _currentCount;
-    }
+	}
 
 	public async void Cutscene1()
 	{
 		MeshInstance3D dark = GetParent().GetNode<MeshInstance3D>("Head/Camera3D/Dark");
 		GD.Print(dark);
 		for (float i = 0; i < 2; i++)
-        {
-          	dark.Visible = true;
+		{
+		  	dark.Visible = true;
 			await ToSignal(GetTree().CreateTimer(0.1f), SceneTreeTimer.SignalName.Timeout);
 			dark.Visible = false; 
 			await ToSignal(GetTree().CreateTimer(0.05f), SceneTreeTimer.SignalName.Timeout);
-        }
+		}
 		await ToSignal(GetTree().CreateTimer(0.5f), SceneTreeTimer.SignalName.Timeout);
 		GetParent().GetParent<fusePathHandler>()._lightsOff = true;
 		await ToSignal(GetTree().CreateTimer(0.5f), SceneTreeTimer.SignalName.Timeout);
@@ -212,15 +212,15 @@ public partial class Ui : Control
 		_text.Text = _lineTable[_currentLine];
 		GetNode<Control>("Dialouge").Visible = true;
 		_inCutscene = false;
-    }
+	}
 
 	public async void Cutscene2()
-    {
-        await ToSignal(GetTree().CreateTimer(1f), SceneTreeTimer.SignalName.Timeout);
+	{
+		await ToSignal(GetTree().CreateTimer(1f), SceneTreeTimer.SignalName.Timeout);
 		_currentLine++;
 		Type();
 		_text.Text = _lineTable[_currentLine];
 		GetNode<Control>("Dialouge").Visible = true;
 		_inCutscene = false;
-    }
+	}
 }
--- off-the-grid/Scripts/fusePathHandler.cs
+++ off-the-grid/Scripts/fusePathHandler.cs
@@ -4,202 +4,202 @@ using System.Linq;
 
 public partial class fusePathHandler : Node3D
 {
-    [Export]
-    public PackedScene PathMeshScene { get; set; }
+	[Export]
+	public PackedScene PathMeshScene { get; set; }
 
 	[Export]
-    public PackedScene FuseScene { get; set; }
-
-    [Export]
-    public PackedScene DarkScene { get; set; }
-
-    [Export]
-    public PackedScene LightScene { get; set; }
-
-    [Export]
-    public float MeshSpacing { get; set; } = 1.0f;
+	public PackedScene FuseScene { get; set; }
 
 	[Export]
-    public int MaxFuses { get; set; } = 5;
+	public PackedScene DarkScene { get; set; }
 
-    public static fusePathHandler Instance { get; private set; }
+	[Export]
+	public PackedScene LightScene { get; set; }
+
+	[Export]
+	public float MeshSpacing { get; set; } = 1.0f;
+
+	[Export]
+	public int MaxFuses { get; set; } = 5;
+
+	public static fusePathHandler Instance { get; private set; }
 	private int _count = 0;
 	private float _closestDist = 10000;
 	private Node3D _closestFuse;
-    private Rid _navigationMap;
+	private Rid _navigationMap;
 	private RandomNumberGenerator _rng = new RandomNumberGenerator();
-    private List<Node3D> _spawnedMeshes = new List<Node3D>();
-    private List<Node3D> _spawnedFuses = new List<Node3D>();
-    private Player _player;
-    private int _maxFuseOG;
-    public bool _isLight = true;
-    public bool _lightsOff = false;
-    private AudioStreamPlayer _sound;
-    public override void _Ready()
-    {
-        Instance = this;
-        _player = GetNode<Player>("Player");
-        _sound = GetNode<AudioStreamPlayer>("Music");
-        _maxFuseOG = MaxFuses;
-        _rng.Randomize();
-        _spawnedFuses = GetNode<Node3D>("FuseSpawns").GetChildren().OfType<Node3D>().ToList();
-        _navigationMap = GetWorld3D().NavigationMap;
-        while (MaxFuses > 0 && _spawnedFuses.Count > 0)
-        {
-            int chosen = _rng.RandiRange(0, _spawnedFuses.Count - 1);
-            Node3D spawn = _spawnedFuses[chosen];
+	private List<Node3D> _spawnedMeshes = new List<Node3D>();
+	private List<Node3D> _spawnedFuses = new List<Node3D>();
+	private Player _player;
+	private int _maxFuseOG;
+	public bool _isLight = true;
+	public bool _lightsOff = false;
+	private AudioStreamPlayer _sound;
+	public override void _Ready()
+	{
+		Instance = this;
+		_player = GetNode<Player>("Player");
+		_sound = GetNode<AudioStreamPlayer>("Music");
+		_maxFuseOG = MaxFuses;
+		_rng.Randomize();
+		_spawnedFuses = GetNode<Node3D>("FuseSpawns").GetChildren().OfType<Node3D>().ToList();
+		_navigationMap = GetWorld3D().NavigationMap;
+		while (MaxFuses > 0 && _spawnedFuses.Count > 0)
+		{
+			int chosen = _rng.RandiRange(0, _spawnedFuses.Count - 1);
+			Node3D spawn = _spawnedFuses[chosen];
 
-            Node3D meshInstance = (Node3D)FuseScene.Instantiate();
-            GetNode<Node3D>("FuseHolder").AddChild(meshInstance);
-            meshInstance.GlobalPosition = spawn.GlobalPosition;
+			Node3D meshInstance = (Node3D)FuseScene.Instantiate();
+			GetNode<Node3D>("FuseHolder").AddChild(meshInstance);
+			meshInstance.GlobalPosition = spawn.GlobalPosition;
 
-            Lighting(LightScene, "Light");
-            _isLight = true;
+			Lighting(LightScene, "Light");
+			_isLight = true;
 
-            _spawnedFuses.RemoveAt(chosen);
-            MaxFuses--;
-        }
-    }
+			_spawnedFuses.RemoveAt(chosen);
+			MaxFuses--;
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
-    {
-        _count += 2;
-        if (_player._collectedFuses >= _maxFuseOG && !_isLight)
-        {
-            Lighting(LightScene, "Light");
-            _sound.Stop();
-            _isLight = true;
-            _player.GetNode<Ui>("UI")._currentLine = 9;
-            _player.GetNode<Ui>("UI").Type();
-            _player.GetNode<Ui>("UI")._text.Text = _player.GetNode<Ui>("UI")._lineTable[_player.GetNode<Ui>("UI")._currentLine];
-            _player.GetNode<Control>("UI/Dialouge").Visible = true;
-        }
-        else if (_player._collectedFuses < _maxFuseOG && _isLight && _lightsOff)
-        {
-            Lighting(DarkScene, "Dark");
-            _sound.Play();
-            _isLight = false;
-        }
+	{
+		_count += 2;
+		if (_player._collectedFuses >= _maxFuseOG && !_isLight)
+		{
+			Lighting(LightScene, "Light");
+			_sound.Stop();
+			_isLight = true;
+			_player.GetNode<Ui>("UI")._currentLine = 9;
+			_player.GetNode<Ui>("UI").Type();
+			_player.GetNode<Ui>("UI")._text.Text = _player.GetNode<Ui>("UI")._lineTable[_player.GetNode<Ui>("UI")._currentLine];
+			_player.GetNode<Control>("UI/Dialouge").Visible = true;
+		}
+		else if (_player._collectedFuses < _maxFuseOG && _isLight && _lightsOff)
+		{
+			Lighting(DarkScene, "Dark");
+			_sound.Play();
+			_isLight = false;
+		}
 
-        if (_count == 100)
-        {
-            _closestDist = 10000;
-            if (!_player._hasFuse)
-            {
-                foreach (Node3D fuse in GetNode<Node3D>("FuseHolder").GetChildren())
-                {
-                    if ((_player.GlobalPosition - fuse.GlobalPosition).Length() < _closestDist)
-                    {
-                        _closestDist = (_player.GlobalPosition - fuse.GlobalPosition).Length();
-                        _closestFuse = fuse;
-                    }
-                }
-            }
-            else
-            {
-                _closestFuse = GetNode<Node3D>("FuseBox");
-            }
-           	GenerateAndPlaceMeshes(_player.GlobalPosition, _closestFuse.GlobalPosition); 
-        }
+		if (_count == 100)
+		{
+			_closestDist = 10000;
+			if (!_player._hasFuse)
+			{
+				foreach (Node3D fuse in GetNode<Node3D>("FuseHolder").GetChildren())
+				{
+					if ((_player.GlobalPosition - fuse.GlobalPosition).Length() < _closestDist)
+					{
+						_closestDist = (_player.GlobalPosition - fuse.GlobalPosition).Length();
+						_closestFuse = fuse;
+					}
+				}
+			}
+			else
+			{
+				_closestFuse = GetNode<Node3D>("FuseBox");
+			}
+		   	GenerateAndPlaceMeshes(_player.GlobalPosition, _closestFuse.GlobalPosition); 
+		}
 		if (_count == 150)
-        {
-            foreach (GpuParticles3D warn in _spawnedMeshes)
-            {
-                warn.Emitting = false;
-            }
+		{
+			foreach (GpuParticles3D warn in _spawnedMeshes)
+			{
+				warn.Emitting = false;
+			}
 			_count = 0;
-        }
+		}
 		if (_count == 200)
-        {
-            ClearMeshes();
+		{
+			ClearMeshes();
 			_count = 0;
-        }
-    }
+		}
+	}
 
-    public void GenerateAndPlaceMeshes(Vector3 startPosition, Vector3 targetPosition)
-    {
-        Vector3[] pathPoints = NavigationServer3D.MapGetPath(
-            _navigationMap,
-            startPosition,
-            targetPosition,
-            true 
-        );
+	public void GenerateAndPlaceMeshes(Vector3 startPosition, Vector3 targetPosition)
+	{
+		Vector3[] pathPoints = NavigationServer3D.MapGetPath(
+			_navigationMap,
+			startPosition,
+			targetPosition,
+			true 
+		);
 
-        if (pathPoints.Length == 0)
-        {
-            GD.Print("Fail");
-            return;
-        }
-        PlaceMeshesAlongPath(pathPoints);
-    }
+		if (pathPoints.Length == 0)
+		{
+			GD.Print("Fail");
+			return;
+		}
+		PlaceMeshesAlongPath(pathPoints);
+	}
 
-    private void PlaceMeshesAlongPath(Vector3[] pathPoints)
-    {
-        float distanceTraveled = 0f;
-        
-        for (int i = 0; i < pathPoints.Length - 1; i++)
-        {
-            Vector3 currentPos = pathPoints[i];
-            Vector3 nextPos = pathPoints[i + 1];
-            float segmentLength = currentPos.DistanceTo(nextPos);
+	private void PlaceMeshesAlongPath(Vector3[] pathPoints)
+	{
+		float distanceTraveled = 0f;
+		
+		for (int i = 0; i < pathPoints.Length - 1; i++)
+		{
+			Vector3 currentPos = pathPoints[i];
+			Vector3 nextPos = pathPoints[i + 1];
+			float segmentLength = currentPos.DistanceTo(nextPos);
 
-            while (distanceTraveled < segmentLength)
-            {
-                Vector3 spawnPosition = currentPos.Lerp(nextPos, distanceTraveled / segmentLength);
+			while (distanceTraveled < segmentLength)
+			{
+				Vector3 spawnPosition = currentPos.Lerp(nextPos, distanceTraveled / segmentLength);
 
-                Node3D meshInstance = (Node3D)PathMeshScene.Instantiate();
-                AddChild(meshInstance);
+				Node3D meshInstance = (Node3D)PathMeshScene.Instantiate();
+				AddChild(meshInstance);
 				meshInstance.GlobalPosition = spawnPosition;
-                _spawnedMeshes.Add(meshInstance);
+				_spawnedMeshes.Add(meshInstance);
 				
-                distanceTraveled += MeshSpacing;
-            }
+				distanceTraveled += MeshSpacing;
+			}
 
-            distanceTraveled -= segmentLength;
-        }
-    }
+			distanceTraveled -= segmentLength;
+		}
+	}
 
-    private void ClearMeshes()
-    {
-        foreach (var mesh in _spawnedMeshes)
-        {
-            mesh.QueueFree();
-        }
-        _spawnedMeshes.Clear();
-    }
+	private void ClearMeshes()
+	{
+		foreach (var mesh in _spawnedMeshes)
+		{
+			mesh.QueueFree();
+		}
+		_spawnedMeshes.Clear();
+	}
 
-    private void Lighting(PackedScene lightingScene, string sceneName)
-    {
-        Node3D holder = GetNode<Node3D>("EnviormentHolder");
-        foreach (Node3D env in GetNode<Node3D>("EnviormentHolder").GetChildren()){ env.QueueFree(); }
+	private void Lighting(PackedScene lightingScene, string sceneName)
+	{
+		Node3D holder = GetNode<Node3D>("EnviormentHolder");
+		foreach (Node3D env in GetNode<Node3D>("EnviormentHolder").GetChildren()){ env.QueueFree(); }
 
-        Node3D instance = (Node3D)lightingScene.Instantiate();
-        instance.Name = sceneName;
-        holder.AddChild(instance);
-    }
+		Node3D instance = (Node3D)lightingScene.Instantiate();
+		instance.Name = sceneName;
+		holder.AddChild(instance);
+	}
 
-    public Aabb GetAllAabb(Node3D parent)
-    {
-        Aabb _combinedAabb = new Aabb();
-        bool first = true;
+	public Aabb GetAllAabb(Node3D parent)
+	{
+		Aabb _combinedAabb = new Aabb();
+		bool first = true;
 
-        foreach (Node child in GetChildren())
-        {
-            if (child is VisualInstance3D visualChild)
-            {
-                Aabb _childAabb = visualChild.GetAabb();
-                Aabb transformedAabb = parent.GlobalTransform.Inverse() * visualChild.GlobalTransform * _childAabb;
-                if (first)
-                {
-                    _combinedAabb = transformedAabb;
-                    first = false;
-                }
-                else
-                {
-                    _combinedAabb = _combinedAabb.Merge(transformedAabb);
-                }
-            }
-        }
-        return _combinedAabb;
-    }
+		foreach (Node child in GetChildren())
+		{
+			if (child is VisualInstance3D visualChild)
+			{
+				Aabb _childAabb = visualChild.GetAabb();
+				Aabb transformedAabb = parent.GlobalTransform.Inverse() * visualChild.GlobalTransform * _childAabb;
+				if (first)
+				{
+					_combinedAabb = transformedAabb;
+					first = false;
+				}
+				else
+				{
+					_combinedAabb = _combinedAabb.Merge(transformedAabb);
+				}
+			}
+		}
+		return _combinedAabb;
+	}
 }
