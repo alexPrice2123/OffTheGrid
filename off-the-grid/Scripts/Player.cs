d815686c61bef0db6a8c0abafdfbca39504a0e92
alexPrice2123 <alexander.price2123@gmail.com>
2026-02-08 13:20:54 -0700

    oppsies (fix)

--- off-the-grid/Scripts/Player.cs
+++ off-the-grid/Scripts/Player.cs
@@ -134,7 +134,7 @@ public partial class Player : CharacterBody3D
 		else if (_dist < 10) { _heartBeat.PitchScale = 1.5f; }
 		else if (_dist < 15) { _heartBeat.PitchScale = 1.25f; }
 		else { _heartBeat.PitchScale = 1; }
-		if (_dist < 2500 && !_heartBeat.Playing) { Beat(); }
+		if (_dist < 25 && !_heartBeat.Playing) { Beat(); }
 		
 		// Add the gravity.
 		if (!IsOnFloor())
